e53be6f35541b7d8eec76db6d5f492d2f3fe36f4
neelmani15 <neelmani242@gmail.com>
2024-05-22 01:56:44 +0530

    Added attendes in the frontend form and demo email for bot in backend

--- Backend1/Controllers/user.js
+++ Backend1/Controllers/user.js
@@ -6,6 +6,13 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const User = require('../Models/userSchema.js');
 const Meeting=require('../Models/MeetRecord.js');
+const { parseISO, subMinutes, addMinutes, isAfter,isBefore } = require('date-fns');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const { executablePath } = require('puppeteer');
+const fs = require('fs');
+const uploadToS3 = require('../Connection/uploadToS3.js');
+const { launch, getStream,wss } = require("puppeteer-stream");
 
 const calendar = google.calendar({
     version:"v3",
@@ -24,99 +31,9 @@ const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const { fromEnv } = require('@aws-sdk/credential-provider-env');
 
-// async function HandleScheduleEvent(req,res){
-//     console.log(req.body.formData);
-
-//     const attendeesEmails = [
-//         { 'email': 'neelmani242@gmail.com' },
-//         // { 'email': 'user2@example.com' }
-//         ];
-//     const { userEmail, formData } = req.body;
-//     const user = await User.findOne({  email: userEmail});
-//     const event = {
-//         summary: req.body.formData.summary,
-//         location: 'Virtual / Google Meet',
-//         description: req.body.formData.description,
-//         start: {
-//           dateTime: req.body.formData.startTime,
-//         },
-//         end: {
-//           dateTime: req.body.formData.endTime,
-//         },
-//         attendees: attendeesEmails,
-//         reminders: {
-//           useDefault: false,
-//           overrides: [
-//             { method: 'email', 'minutes': 24 * 60 },
-//             { method: 'popup', 'minutes': 10 },
-//           ],
-//         },
-//         conferenceData: {
-//           createRequest: {
-//             conferenceSolutionKey: {
-//               type: 'hangoutsMeet'
-//             },
-//             requestId: 'coding-calendar-demo'
-//           }
-//         },
-//       };
-      
-
-//       try {
-//         const response = await calendar.events.insert({
-//             calendarId: 'primary',
-//             auth:oauth2Client,
-//             resource: event,
-//             conferenceDataVersion: 1
-//         });
-
-//         console.log(response);
-
-//         const summary = response.data.summary;
-//         const description = response.data.description;
-//         const location = response.data.location;
-//         const scheduleStartTime = response.data.start.dateTime;
-//         const scheduleEndTime = response.data.end.dateTime;
-//         const meetinglink = response.data.hangoutLink;
-//         const attendees = response.data.attendees;
-//         const alldata={
-//             summary:summary,
-//             description:description,
-//             start:scheduleStartTime,
-//             end:scheduleEndTime,
-//             url:meetinglink,
-//             // attendees:attendees
-//         }
-
-//         user.events.push(alldata);
-//         await user.save();
-        
-    
-//         // const { data: { summary, location, start, end, attendees }, config: { data: { conferenceData } } } = response;
-    
-//         // Get the Google Meet conference URL in order to join the call
-//         // const { uri } = conferenceData.entryPoints[0];
-//         console.log(`📅 Calendar event created: ${summary} at ${location}, from ${scheduleStartTime} to ${scheduleEndTime}, attendees:\n${attendees.map(person => `🧍 ${person.email}`).join('\n')} \n 💻 Join conference call link: ${meetinglink}`);
-//         res.send({
-//             message:"Event Added"
-//         })
-//     } catch (error) {
-//         console.error("Error:", error.message);
-//         if (error.response && error.response.data) {
-//             console.error("Google Calendar API error:", error.response.data);
-//             res.status(404).json({ message: 'Google Calendar API Error.' });
-//         }else{
-//             res.status(404).json({ message: 'Unable to add the Event' });
-//         }
-//     }
-// }
 async function HandleScheduleEvent(req, res) {
   console.log("schedule  executed");
 
-  const attendeesEmails = [
-      { email: 'neelmani242@gmail.com' },
-      // { 'email': 'user2@example.com' }
-  ];
 
   const { dataToSubmit } = req.body; // Destructure userEmail and formData from the request body
     console.log(dataToSubmit)
@@ -137,6 +54,18 @@ async function HandleScheduleEvent(req, res) {
           version: 'v3',
           auth: oauth2Client
       });
+
+      let attendeesEmails = [
+        { email: 'riktamaibot@gmail.com' }
+    ];
+
+    // Add additional attendees if they exist
+    if (dataToSubmit.attendees && dataToSubmit.attendees.length > 0) {
+        const additionalAttendees = dataToSubmit.attendees.map(email => ({ email }));
+        attendeesEmails = attendeesEmails.concat(additionalAttendees);
+    }
+
+      console.log(attendeesEmails);
 
       const event = {
           summary: dataToSubmit.summary,
@@ -172,7 +101,7 @@ async function HandleScheduleEvent(req, res) {
           conferenceDataVersion: 1
       });
 
-      console.log(response);
+    //   console.log(response);
 
       const summary = response.data.summary;
       const description = response.data.description;
@@ -181,6 +110,7 @@ async function HandleScheduleEvent(req, res) {
       const scheduleEndTime = response.data.end.dateTime;
       const meetinglink = response.data.hangoutLink;
       const attendees = response.data.attendees;
+      const userEmail = dataToSubmit.userEmail;
       const alldata = {
           summary: summary,
           description: description,
@@ -192,6 +122,34 @@ async function HandleScheduleEvent(req, res) {
 
       user.events.push(alldata);
       await user.save();
+
+      const startTime = parseISO(scheduleStartTime);
+      const endTime = parseISO(scheduleEndTime);
+      const botJoinTime = subMinutes(startTime, 5);
+      const interval = 1 * 60 * 1000; // 1 minutes in milliseconds
+
+      let joined = false;
+
+      const checkAndJoinMeeting = async () => {
+        if (joined) {
+            clearInterval(intervalId);
+            joined=false;
+            return;
+        }
+          const now = new Date();
+        if (isAfter(now, startTime) && isBefore(now, endTime)) {
+            joined = await HandlejoinMeeting(meetinglink, userEmail);
+            if (joined) {
+                clearInterval(intervalId);
+                joined=false;
+            }
+        } else if (isAfter(now, endTime)) {
+            clearInterval(intervalId);
+            joined=false;
+        }
+    };
+
+      const intervalId = setInterval(checkAndJoinMeeting, interval);
 
       console.log(`📅 Calendar event created: ${summary} at ${location}, from ${scheduleStartTime} to ${scheduleEndTime}, attendees:\n${attendees.map(person => `🧍 ${person.email}`).join('\n')} \n 💻 Join conference call link: ${meetinglink}`);
       res.send({
@@ -206,6 +164,188 @@ async function HandleScheduleEvent(req, res) {
           res.status(500).json({ message: 'Unable to add the Event' });
       }
   }
+}
+
+async function HandlejoinMeeting(meetUrl, userEmail) {
+    console.log("Joining Meet");
+    // const { meetUrl } = req.body;
+    console.log(meetUrl);
+    console.log(userEmail);
+    const parts = meetUrl.split('/');
+    const meetingId = parts[parts.length - 1];
+    // const file = fs.createWriteStream("./report/test2.mp4");
+
+    console.log(meetingId);
+
+    try {
+        puppeteer.use(StealthPlugin());
+        const browser = await launch(puppeteer, {
+            defaultViewport: null,
+            headless: true,
+            devtools: false,
+            args: [
+                "--autoplay-policy=no-user-gesture-required",
+            ],
+            executablePath: executablePath(),
+        });
+        const page = (await browser.pages())[0];
+        
+        // Define the path for storing the recorded file
+        // const filePath = './report/video/meeting_recording.webm';
+        const filePath = `./report/video/meetingId_${meetingId}.webm`;
+        console.log(filePath)
+        const fileStream = fs.createWriteStream(filePath);
+        
+        // Get the media stream
+        const stream = await getStream(page, { audio: true, video: true });
+        stream.pipe(fileStream);
+        // stream.pipe(file);
+
+        const navigationPromise = page.waitForNavigation();
+        const context = browser.defaultBrowserContext();
+        await context.overridePermissions("https://meet.google.com/", ["microphone", "camera", "notifications"]);
+        
+        await page.goto(meetUrl, { waitUntil: "networkidle0", timeout: 120000 });
+        await navigationPromise;
+
+        await page.waitForSelector('input[aria-label="Your name"]', { visible: true, timeout: 50000 });
+        console.log('Name input found');
+        await page.type('input[aria-label="Your name"]', 'riktam.ai NoteTaker');
+
+        try {
+            const cameraButtonSelector = '[aria-label*="Turn off camera"]';
+            const microphoneButtonSelector = '[aria-label*="Turn off microphone"]';
+            
+            await page.waitForSelector(cameraButtonSelector, { visible: true, timeout: 60000 });
+            console.log('Camera button found');
+            await page.click(cameraButtonSelector);
+            console.log('Camera turned off');
+            
+            await page.waitForSelector(microphoneButtonSelector, { visible: true, timeout: 60000 });
+            console.log('Microphone button found');
+            await page.click(microphoneButtonSelector);
+            console.log('Microphone turned off');
+
+        } catch (err) {
+            console.error('Error turning off camera/microphone:', err);
+        }
+
+        const askToJoinButtonSelector = 'button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 jEvJdc QJgqC"]';
+        await page.waitForSelector(askToJoinButtonSelector, { visible: true, timeout: 50000 });
+        console.log('Ask to join button found');
+        await page.click(askToJoinButtonSelector);
+        console.log('Clicked on Ask to join button');
+
+        page.on('framenavigated', async (frame) => {
+            const url = frame.url();
+            console.log(url);
+            if (!url.includes('meet.google.com')) {
+                console.log("Meeting Stopped");
+                await HandleStopRecording(browser, stream, fileStream,meetingId,userEmail);
+                return
+            }
+        });
+
+        setInterval(async () => {
+           let answer=await HandleCheckBotPresence(page, browser, stream, fileStream,meetingId,userEmail);
+           if (answer){
+            stop=true
+            return
+           }
+        }, 10000);
+        return true;
+
+        // res.status(200).json({ message: 'Recording started successfully.' });
+
+    } catch (error) {
+        console.error('Error starting recording:', error);
+        return false;
+        // res.status(500).json({ error: 'An error occurred while starting recording.' });
+    }
+}
+
+async function HandleStopRecording(browser, stream, fileStream,meetingId,userEmail) {
+    try {
+        stream.unpipe(fileStream);
+        fileStream.end();
+        console.log("Recording stopped successfully.");
+        const s3Url = await uploadToS3(fileStream.path, 'riktam-recordings',meetingId);
+        console.log(s3Url)
+        // const s3Url = await uploadToS3(fileStream.path, 'riktam-recordings',meetingId);
+        // console.log(s3Url)
+        // isRecordingStopped=true
+        const meetingRecord = new Meeting({
+            userEmail: userEmail,
+            meetingId: meetingId,
+            videoS3url: s3Url
+        });
+        
+        // Save the meeting record to the database
+        await meetingRecord.save();
+        
+        console.log("Meeting record saved successfully.");
+        
+        await browser?.close();
+        console.log("browser closed")
+        isRecordingStopped=true
+        
+    } catch (error) {
+        console.error('Error stopping recording:', error);
+    }
+}
+
+let gotItClicked = false;
+let isRecordingStopped=false
+async function HandleCheckBotPresence(page, browser, stream, fileStream,meetingId,userEmail) {
+    try {
+        const botName = 'riktam.ai NoteTaker'; // Adjust this to match the bot's name
+        if (isRecordingStopped) {
+            return true;
+        } else {
+            const frame = page.mainFrame();
+            if (!frame.isDetached()) {
+                const { participants, leftMeetingText } = await frame.evaluate(() => {
+                    const leftMeetingElement = document.querySelector('h1[jsname="r4nke"].roSPhc');
+                    const leftMeetingText = leftMeetingElement ? leftMeetingElement.textContent : null;
+                    const participants = [];
+                    const participantElements = document.querySelectorAll('div.dwSJ2e');
+                    participantElements.forEach(participant => {
+                        participants.push(participant.innerText);
+                    })
+                    return { participants, leftMeetingText };
+                });
+
+                console.log('Participants:', participants);
+                console.log('Meeting status:', leftMeetingText);
+
+                if (leftMeetingText) {
+                    await HandleStopRecording(browser, stream, fileStream,meetingId,userEmail);
+                    return;
+                }
+
+                if (participants.length >= 1) {
+                    if (!gotItClicked) {
+                        await page.click('span[jsname="V67aGc"].mUIrbf-vQzf8d');
+                        console.log('Clicked on "Got it" button');
+                        gotItClicked = true;
+                    }
+                }
+
+                if (participants.length === 1) {
+                    gotItClicked = false;
+                    await HandleStopRecording(browser, stream, fileStream,meetingId,userEmail);
+                    return;
+                }
+            }
+        }
+    } catch (error) {
+        if (error instanceof puppeteer.errors.TargetCloseError) {
+            console.error('Target closed. Terminating gracefully.');
+            await HandleStopRecording(browser, stream, fileStream);
+        } else {
+            console.error('Error checking bot presence:', error);
+        }
+    }
 }
 
 async function HandelEventList(req, res) {
